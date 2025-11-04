b5330a0881c85626008008c3fda5e3fc5fd35e73
saassteve <166345757+saassteve@users.noreply.github.com>
2025-11-04 14:20:18 +0000

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sanctum-cyan': {
+          300: '#87CFE1',
+          400: '#5EC4DB',
+          500: '#35B9D5',
+          600: '#2A94AC',
+          700: '#1F6F83',
+        },
+        'sanctum-gray': {
+          800: '#1E293B',
+        },
+      },
+    },
   },
   plugins: [],
 };
